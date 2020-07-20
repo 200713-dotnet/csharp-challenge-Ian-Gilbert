bb8da4f4a3835f0427e2c7074ed614474e64925b
Ian Gilbert <ian.d.gilbert@gmail.com>
2020-07-20 13:43:27 -0700

    initial commit. Added paldrome checker method, no testing yet

--- Palindrome.Domain/Class1.cs
+++ Palindrome.Domain/Class1.cs
@@ -4,5 +4,39 @@ namespace Palindrome.Domain
 {
     public class Class1
     {
+        public static bool PalindromeChecker(string input)
+        {
+            string forwards = new StringBuilder();
+            string reverse = new StringBuilder();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (
+                    (input[i] >= '0' && input[i] <= '9')
+                    || (input[i] >= 'A' && input[i] <= 'z')
+                )
+                {
+                    forwards.Append(input[i]);
+                }
+
+                var reverse_index = input.Length - i - 1;
+                if (
+                    (input[reverse_index] >= '0' && input[reverse_index] <= '9')
+                    || (input[reverse_index] >= 'A' && input[reverse_index] <= 'z')
+                )
+                {
+                    reverse.Append(input[i]);
+                }
+
+                if (forwards.Equals(reverse))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
